844cfc8162925650195083552bd6b65b11baaedd
apere <apere@LAPTOP-IEIE8HM7>
2022-07-28 20:36:34 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var process in Process.GetProcessesByName(args[0]))
+            {
+                if ((DateTime.Now - process.StartTime).TotalMilliseconds > int.Parse(args[1])*1000)
+                process.Kill();
+                System.Threading.Thread.Sleep(int.Parse(args[2]) * 1000);
+            }
+
+        }
+    }
+
+    
+}
