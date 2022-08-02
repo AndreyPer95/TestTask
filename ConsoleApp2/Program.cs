8f05a1b69834680c16703252df41f71e450c48d0
AndreyPer95 <a.peresypkin2201@gmail.com>
2022-08-02 22:18:14 +0500

    validity check

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,31 +12,34 @@ namespace KillUtilite
 {
     class Program
     {
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("Unhandled Exception: " + Environment.NewLine + e.ExceptionObject.ToString());
+        }
         static void Main(string[] args)
         {
+            
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
             var nameProcess = args[0];
             var admissibleLifeTime = int.Parse(args[1]);
             var frequency = int.Parse(args[2]);
             var admissible = new TimeSpan(0, admissibleLifeTime, 0);
-            while (true)
-            {
 
-                foreach (var process in Process.GetProcessesByName(nameProcess))
+            foreach (var process in Process.GetProcessesByName(nameProcess))
+            {
+                var startTime = process.StartTime;
+                var timeNow = DateTime.Now;
+                var processLifeTime = timeNow.Subtract(startTime);
+                if (processLifeTime > admissible)
                 {
-                    var startTime = process.StartTime;
-                    var timeNow = DateTime.Now;
-                    var processLifeTime = timeNow.Subtract(startTime);
-                    if (processLifeTime > admissible)
-                    {
-                        process.Kill();
-                        break;
-                    }
+                    process.Kill();
+                    break;
                 }
-                Thread.Sleep(frequency * 60000);
             }
+            Thread.Sleep(frequency * 60000);
 
         }
     }
 
-    
+
 }
